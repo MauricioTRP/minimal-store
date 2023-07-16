be5366e8bdd36820debda736f2fbb81199c5e912
Mauricio <mauriciofb@gmail.com>
2023-07-16 00:39:39 -0400

    refactor home#index shared/navbar and tests

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    assign(:products, [
+      create(:product),
+      create(:product)
+    ])
+  end
+
+  it 'renders a navbar' do
+    render 'shared/navbar'
+
+    assert_select 'nav', minimum: 1
+  end
+
+  it 'renders products' do
+    render
+
+    assert_select 'h5', minimum: 2
+  end
 end
