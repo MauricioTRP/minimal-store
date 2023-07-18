ab72d1e5e2e5a0f9595e78bb26914843365e6064
Mauricio <mauriciofb@gmail.com>
2023-07-18 00:20:09 -0400

    Added validations and factorie to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
 
   # ========== Validations =====================
   validates_with RutValidator
+
+  name_rgx = /[a-z\s.-]/i
+  phone_rgx =  /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/
+  name_message = "Only Letters and symbols , . ' - "
+  phone_message = 'Invalid phone number'
+  validates :name, presence: true, format: { with: name_rgx, message: name_message }
+  validates :last_name, presence: true, format: { with: name_rgx, message: name_message }
+  validates :phone, presence: true, format: { with: phone_rgx, message: phone_message }
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test-#{n.to_s.rjust(3,'0')}@example.com"}
     password { "123456" }
+    name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone { Faker::PhoneNumber.cell_phone }
+    rut { Faker::ChileRut.full_rut }
   end
 end
