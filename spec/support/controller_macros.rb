e4d18d2645eef39985ebcfc6d17e5b33cacc4882
Mauricio <mauriciofb@gmail.com>
2023-07-22 13:42:25 -0400

    Added devise controller helpers and integration helpers, pending to add specs for requests

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,8 +1,15 @@
 module ControllerMacros
+  def login_admin
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in create(:admin) # Using factory bot as an example
+    end
+  end
+
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryBot.create(:user)
+      user = create(:user)
       sign_in user
     end
   end
