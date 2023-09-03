48ee67662a0a3b34f036d0e89d580f87a5f4f300
Mauricio <mauriciofb@gmail.com>
2023-09-02 21:58:59 -0400

    Select drop down to payment type

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order < ApplicationRecord
   # ========== Validations =====================
   name_rgx = /[a-z\s.-]/i
   name_message = "Only Letters and symbols , . ' - "
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
 
   enum pay_type: {
     "Check": 0,
